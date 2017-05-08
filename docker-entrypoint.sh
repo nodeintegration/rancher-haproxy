c990c9ca237ef1c742b858cf6cd6993a40afeea2
Brendan Beveridge <brendan@nodeintegration.com.au>
2017-05-08 16:57:09 +1000

    add line separator for ssl certs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,11 +24,15 @@ if [ "$1" = "haproxy" ]; then
         echo "[INFO]: getting base64 encoded ssl certificate from metadata http://${RANCHER_API_HOST}/${RANCHER_API_VERSION}/self/service/metadata/ssl_cert"
         curl -sS http://${RANCHER_API_HOST}/${RANCHER_API_VERSION}/self/service/metadata/ssl_cert | base64 -d > ${HAPROXY_SSL_CERT}
 
+        echo -e "\n" >> ${HAPROXY_SSL_CERT}
+
         echo "[INFO]: getting base64 encoded ssl key from metadata http://${RANCHER_API_HOST}/${RANCHER_API_VERSION}/self/service/metadata/ssl_key"
         curl -sS http://${RANCHER_API_HOST}/${RANCHER_API_VERSION}/self/service/metadata/ssl_key | base64 -d >> ${HAPROXY_SSL_CERT}
       else
         echo "[INFO]: getting ssl certificate from metadata http://${RANCHER_API_HOST}/${RANCHER_API_VERSION}/self/service/metadata/ssl_cert"
         curl -sS http://${RANCHER_API_HOST}/${RANCHER_API_VERSION}/self/service/metadata/ssl_cert > ${HAPROXY_SSL_CERT}
+
+        echo -e "\n" >> ${HAPROXY_SSL_CERT}
 
         echo "[INFO]: getting ssl key from metadata http://${RANCHER_API_HOST}/${RANCHER_API_VERSION}/self/service/metadata/ssl_key"
         curl -sS http://${RANCHER_API_HOST}/${RANCHER_API_VERSION}/self/service/metadata/ssl_key >> ${HAPROXY_SSL_CERT}
